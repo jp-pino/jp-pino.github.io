33904e486953983047169921365dcdab2b04291b
Juan Pablo Pino Bravo <jp.pinob@gmail.com>
2023-12-03 20:27:16 +0100

    Add build badges

--- generate.js
+++ generate.js
@@ -23,6 +23,8 @@ async function getRepos() {
     let langs = await req.json();
     let main_lang = Object.keys(langs)[0];
 
+    let badge_urls = (await (await fetch(`https://api.github.com/repos/jp-pino/${project.name}/actions/workflows`, config)).json()).workflows.map((workflow) => workflow.badge_url);
+
     return {
       name: project.name,
       full_name: project.full_name,
@@ -38,6 +40,7 @@ async function getRepos() {
       created_at: project.created_at,
       updated_at: project.updated_at,
       language: main_lang,
+      badge_urls: badge_urls
     }
   }));
 
@@ -50,14 +53,18 @@ async function getRepos() {
 }
 
 getRepos().then((projects) => {
-  let html = ``;
+  let html = "";
 
   projects.forEach((project) => {
     html += `
   <div class="group w-72 bg-gray-100 border rounded-lg border-gray-700 p-4 shadow hover:bg-gray-700 duration-200 mb-2 hover:shadow-lg">
-    <a href="${project.url}" class="flex flex-row">
+    <a href="${project.url}" class="group flex flex-row relative">
+    `;
+    html += `
       <span class="text-gray-800 group-hover:text-gray-300 group-hover:hover:text-white font-semibold"><i class="fa-solid fa-star ${project.stars > 0 ? "" : "hidden"}" style="color: #fecb3e;"></i> ${project.full_name}<span>
     </a>`;
+
+
     if (project.description) {
       html += `
     <p class="text-xs text-gray-500 mt-3">
@@ -82,9 +89,19 @@ getRepos().then((projects) => {
         <i class="fa-solid fa-book"></i> Docs
       </a> `;
     }
-    html += `
-    </div >
-  </div > `;
+    html += `</div>`;
+
+    if (project.badge_urls.length > 0) {
+      html += `<div class='hidden group-hover:block bg-gray-300 border-gray-500 rounded-md shadow-md mt-2 border-2 p-1'>`;
+
+      project.badge_urls.forEach((badge_url) => {
+        if (badge_url.includes('pages-build-deployment')) return;
+        html += `<img src="${badge_url}" class="inline-block mr-2" />`;
+      });
+
+      html += `</div>`;
+    }
+    html += `</div>`;
   });
 
   html += `</div > `;
